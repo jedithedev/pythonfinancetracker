3e3b64a1d707bc49289e81e73f4ddaca7c1e3089
sigma970 <godmat08@ecolecatholique.ca>
2025-02-14 15:01:13 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,6 +1,15 @@
 import numpy as np
-import matplotlib as plt
+import plotly.graph_objects as go
 from saveinfo import Info
-
-file.open("finances.json", "r")
-x_points = np.array([0, f.read(1)])
+f.open('finances.json', 'r')
+diagram = go.Figure(data=[
+  go.Sankey
+    node = dict(
+      thickness = 5,
+      label = [i for i in f]
+      color = "grey"
+      ),
+    link = dict(
+      source = [ ],
+      target = [ ],
+      value = [ ]))])
