24234a912b602d7b163e68a44b496984044ac677
jedithedev <77647468+jedithedev@users.noreply.github.com>
2025-02-14 14:07:40 -0500

    Update saveinfo.py
    now, you can save info

--- saveinfo.py
+++ saveinfo.py
@@ -1,1 +1,13 @@
 import json
+
+class Info:
+  def __init__(self, income, expenses):
+    self.income = income
+    self.expenses = expenses
+
+def save(income, expenses, filename):
+  s = Info(income, expenses)
+
+  with open(filename, 'W') as f:
+    json.dump(s, f)
+  
