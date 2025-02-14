86dd11045ed56cb441002d210a1532034125b4ae
jedithedev <77647468+jedithedev@users.noreply.github.com>
2025-02-14 15:00:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import saveinfo
 from saveinfo import Info
 
+fileinfo = {
+  'income' : {
+    'unemployed' : 25,
+    'selling grease' : 25
+  },
+  'expenses' : {
+    'rent (cardboard box)' : 5,
+    'mcdonalds' : 43
+  }
+}
+
 try : 
   file = open('finances.json', 'r')
 except FileNotFoundError:
-  file = saveinfo.filesave(Info(100, 50), 'finances.json')
+  file = saveinfo.filesave(Info(fileinfo['income'], fileinfo['expenses']), 'finances.json')
