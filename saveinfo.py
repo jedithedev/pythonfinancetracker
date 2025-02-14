eb279b9dab22eda29f88727d0269611ce7cd271b
jedithedev <77647468+jedithedev@users.noreply.github.com>
2025-02-14 14:20:02 -0500

    Update saveinfo.py

--- saveinfo.py
+++ saveinfo.py
@@ -5,11 +5,14 @@ class Info:
     self.income = income
     self.expenses = expenses
 
-def save(income, expenses, filename):
-  s = Info(income, expenses)
+def save(s : Info, filename : string):
 
   with open(filename, 'W') as f:
     json.dump(s, f)
 
 def open(filename):
-  
+  file = open(filename, 'r').read()
+  f = json.load(file)
+  s = Info(f['income'], f['expenses'])
+
+  return s
