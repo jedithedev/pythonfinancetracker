da9e2bde75d9b1eba225f8a378d67092133b2973
sigma970 <godmat08@ecolecatholique.ca>
2025-02-18 14:27:41 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,21 +1,23 @@
 import numpy as np
 import plotly.graph_objects as go
 from saveinfo import Info
-f.open('finances.json', 'r')
-diagram = go.Figure(data=[
-  go.Sankey
-    node = dict(
-      thickness = 5,
-      label = [i for i in f]
-      color = "grey"
-      ),
-    link = dict(
-      source = [ ],
-      target = [ ],
-      value = [ ]))])
 
-fig.update_layout(
-  title = name + "'s finance tracker"
-  font = dict(size = 12 color = 'black')
-)
-fig.show()
+def graph():
+  f.open('finances.json', 'r')
+  diagram = go.Figure(data=[
+    go.Sankey(
+      node = dict(
+        thickness = 5,
+        label = [f.income, f.expenses]
+        color = "grey"
+        ),
+      link = dict(
+        source = [],
+        target = [],
+        value = []))])
+  
+  fig.update_layout(
+    title = name + "'s finance tracker"
+    font = dict(size = 12 color = 'black')
+  )
+  fig.show()
