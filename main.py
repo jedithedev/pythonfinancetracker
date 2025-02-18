bd7368f8e1cc78b10bc0fca62c579b67fd955a77
sigma970 <godmat08@ecolecatholique.ca>
2025-02-18 13:43:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import saveinfo
-import graph.py
-from saveinfo import Info
-from graph import json
+import graph
 
 try : 
-  file = saveinfo.fileopen('finances.json')
+  file = fileopen('finances.json')
   print(file)
 except FileNotFoundError:
-  file = saveinfo.filesave(Info(fileinfo['income'], fileinfo['expenses']), 'finances.json')
+  file = filesave(Info(fileinfo['income'], fileinfo['expenses']), 'finances.json')
 
-income_input = input(str())
-
-
-print("What are your incomes?")
-income_input = input(str())
-print(income_input)
-
-print("What are your expenses")
-expenses_input input(str())
-print(expenses_input)
+def user_input():
+  income_input = input(str())
+  
+  print("What are your incomes?")
+  income_input = input(str())
+  print(income_input)
+  
+  print("What are your expenses")
+  expenses_input input(str())
+  print(expenses_input)
 
 finances = {
   'income' : income_input
